d036e706cc1e63c21eacfc2b529ec5d424b2fe81
Abigail Arias <abby@Abigails-MacBook-Air.local>
2015-02-21 13:27:33 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,13 +14,14 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    [self addObstacle];
+
 }
 
 // put new methods here
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
     // this will get called every time the player touches the screen
     [character flap];
-    [self addObstacle];
 }
 
 // declare a method in objective-C
